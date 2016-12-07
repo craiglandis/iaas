084846776aa68d404493601f67bb11e459ea3aa1
craiglandis <craiglandis@users.noreply.github.com>
2016-12-07 15:35:02 -0500

    Add files via upload

--- Get-RdpRegValues.ps1
+++ Get-RdpRegValues.ps1
@@ -1,8 +1,22 @@
-﻿Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name 'PortNumber' -Value '3389'
+﻿param(
+[switch]$setDefault
+)
 
-(Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name 'PortNumber').PortNumber
-(Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server' -Name 'fDenyTSConnections').fDenyTSConnections
+$portNumber = (Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name 'PortNumber').PortNumber
+$fDenyTSConnections = (Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server' -Name 'fDenyTSConnections').fDenyTSConnections
 
-(Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\DomainProfile' -Name 'EnableFirewall').EnableFirewall
-(Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\PublicProfile' -Name 'EnableFirewall').EnableFirewall
-(Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\StandardProfile' -Name 'EnableFirewall').EnableFirewall
+$domainProfile = (Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\DomainProfile' -Name 'EnableFirewall').EnableFirewall
+$publicProfile = (Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\PublicProfile' -Name 'EnableFirewall').EnableFirewall
+$standardProfile = (Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\StandardProfile' -Name 'EnableFirewall').EnableFirewall
+
+write-host "PortNumber: $portNumber"
+write-host "fDenyTSConnections: $fDenyTSConnections"
+write-host "DomainProfile: $domainProfile"
+write-host "publicProfile: $publicProfile"
+write-host "standardProfile: $standardProfile"
+
+if ($setDefault)
+{
+    write-host "Setting PortNumber to 3389 because -setDefault parameter is $setDefault"
+    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name 'PortNumber' -Value '3389'
+}
